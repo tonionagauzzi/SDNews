e55422e4a079d1c81a96176b0064b855a2c98b62
nagauzzi <kensaku.nagauchi@access-company.com>
2021-05-30 23:10:44 +0900

    Implement sample - markdown preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Remarkable } from 'remarkable';
+import { linkify } from 'remarkable/linkify';
+import externalLink from 'remarkable-external-link';
 
 function App() {
+  const remarkable = new Remarkable({
+    html: false,
+    breaks: true,
+    typographer: false,
+  })
+    .use(linkify)
+    .use(externalLink);
+
+  const textAreaStyle = {
+    height: '13em',
+    lineHeight: '1.3',
+    minWidth: '90vh',
+  };
+
+  const outputStyle = {
+    height: '13em',
+    lineHeight: '1.3',
+    minWidth: '90vh',
+    paddingLeft: '5vh',
+    textAlign: 'left',
+  };
+
+  const [input, updateInput] = useState("# My TODO\n1. Eat\n1. Sleep\n1. Have fun\n# Your TODO\n* Work\n* Work\n* Work\n# My Website\n* [tonionagauzzi.github.io](tonionagauzzi.github.io/SDNews)");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Input your Markdown</h1>
+      <textarea
+        id="markdown-content"
+        defaultValue={input}
+        onChange={(e: any) => {
+          updateInput(e.target.value);
+        }}
+        style={textAreaStyle}
+      />
+      <div
+        className="md"
+        dangerouslySetInnerHTML={{ __html: remarkable.render(input) }}
+        style={outputStyle as any}
+      />
     </div>
   );
 }
